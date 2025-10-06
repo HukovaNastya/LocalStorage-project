ff1cbe5cbcb8af321a4d964c34460f4ebcb51ce7
HukovaNastya <hukovanasta@gmail.com>
2025-10-06 17:47:50 +0300

    added-items-in-localStorage

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
-const items = [];
+const items = JSON.parse(localStorage.getItem('items')) || [];
 
 function addItem(e){
     e.preventDefault();
@@ -12,19 +12,21 @@ function addItem(e){
     }
     items.push(item);
     populateList(items, itemsList);
+    localStorage.setItem('items', JSON.stringify(items));
     this.reset();
 }
 
 function populateList(plates = [], platesList){
   platesList.innerHTML = plates.map((plate, index) => {
-  platesList.innerHTML = plates.map((plate, index) => {
       return `
         <li>
-          <input type="checkbox" data-index="${index}" id="item${index}">
+          <input type="checkbox" data-index="${index}" id="item${index}" ${plate.done ? 'checked': null}>
           <label for="item${index}">${plate.text}</label>
         </li>
       `;
   }).join('');
 }
 
-addItems.addEventListener('submit', addItem)
+addItems.addEventListener('submit', addItem);
+
+populateList(items, itemsList);
